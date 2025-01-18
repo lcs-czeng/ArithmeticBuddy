b2df918f046128861a17a6ca625ced2601e6ff05
Courage Zeng <czeng@lcs.on.ca>
2025-01-18 10:24:36 -0500

    Initial Commit

---
+++ ArithmeticBuddy/ArithmeticBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticBuddyApp.swift
+//  ArithmeticBuddy
+//
+//  Created by 曾梓恒 on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
