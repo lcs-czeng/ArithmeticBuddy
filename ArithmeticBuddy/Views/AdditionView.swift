17e5b6a56788e9b4e10748cf80ed0b9e1fcac339
Courage Zeng <czeng@lcs.on.ca>
2025-01-18 10:26:33 -0500

    Created the project and finished initial project organization.

---
+++ ArithmeticBuddy/Views/AdditionView.swift
@@ -0,0 +1,18 @@
+//
+//  AdditionView.swift
+//  ArithmeticBuddy
+//
+//  Created by 曾梓恒 on 2025-01-18.
+//
+
+import SwiftUI
+
+struct AdditionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdditionView()
+}
